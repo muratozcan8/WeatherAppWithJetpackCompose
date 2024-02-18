13228350d75b9825ae0d27eb104fbc34ad6aa258
muratozcan8 <mrtzcn08@gmail.com>
2024-02-18 22:11:38 +0300

    adjusted theme settings

--- app/src/main/java/com/muratozcan/weatherappwithjetpackcompose/ui/theme/Theme.kt
+++ app/src/main/java/com/muratozcan/weatherappwithjetpackcompose/ui/theme/Theme.kt
@@ -14,34 +14,23 @@ import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = Dark200,
+    secondary = Dark500,
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    primary = Dark200,
+    secondary = Dark500,
 )
 
 @Composable
 fun WeatherAppWithJetpackComposeTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
@@ -58,10 +47,10 @@ fun WeatherAppWithJetpackComposeTheme(
         SideEffect {
             val window = (view.context as Activity).window
             window.statusBarColor = colorScheme.primary.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = false
         }
     }
-
+    rememberSystemUiController().isStatusBarVisible = false
     MaterialTheme(
         colorScheme = colorScheme,
         typography = Typography,
