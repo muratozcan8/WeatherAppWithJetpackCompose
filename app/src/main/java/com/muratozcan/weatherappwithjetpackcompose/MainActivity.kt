1cb60e82b219451eb35ecacdec742a27fe9697d6
muratozcan8 <mrtzcn08@gmail.com>
2024-02-20 23:26:28 +0300

    added fonts (ubuntu and russo_one)

--- app/src/main/java/com/muratozcan/weatherappwithjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/muratozcan/weatherappwithjetpackcompose/MainActivity.kt
@@ -3,12 +3,15 @@ package com.muratozcan.weatherappwithjetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.muratozcan.weatherappwithjetpackcompose.ui.theme.WeatherAppWithJetpackComposeTheme
 
@@ -19,8 +22,14 @@ class MainActivity : ComponentActivity() {
             WeatherAppWithJetpackComposeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    modifier = Modifier.fillMaxSize().background(Brush.verticalGradient(
+                        listOf(
+                            MaterialTheme.colorScheme.primary,
+                            MaterialTheme.colorScheme.primary,
+                            MaterialTheme.colorScheme.secondary
+                        )
+                    )),
+                    color = Color.Transparent
                 ) {
                     Navigation()
                 }
