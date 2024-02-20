ed0af9d92992741210f19378b8fdc66c4845cec7
muratozcan8 <mrtzcn08@gmail.com>
2024-02-20 23:58:30 +0300

    api_key was added to local.properties

--- app/src/main/java/com/muratozcan/weatherappwithjetpackcompose/network/Api.kt
+++ app/src/main/java/com/muratozcan/weatherappwithjetpackcompose/network/Api.kt
@@ -7,26 +7,34 @@ import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
+import java.util.Properties
 
-const val APIKEY = ""
 interface Api {
      @GET("locations/v1/cities/search")
      suspend fun searchLocation(
-         @Query("apikey") apiKey: String = APIKEY,
+         @Query("apikey") apiKey: String = getApiKey(),
          @Query("q") query: String
      ):Response<List<Location>>
 
      @GET("forecasts/v1/daily/5day/{location_key}")
      suspend fun getDailyForecast(
          @Path("location_key") locationKey: String,
-         @Query("apikey") apiKey: String = APIKEY,
+         @Query("apikey") apiKey: String = getApiKey(),
          @Query("metric") metric: Boolean = true
      ): Response<DailyForecasts>
 
      @GET("forecasts/v1/hourly/12hour/{location_key}")
      suspend fun getHourlyForecast(
          @Path("location_key") locationKey: String,
-         @Query("apikey") apiKey: String = APIKEY,
+         @Query("apikey") apiKey: String = getApiKey(),
          @Query("metric") metric: Boolean = true
      ): Response<List<HourlyForecast>>
+
+    fun getApiKey(): String {
+        val properties = Properties()
+        val inputStream = this::class.java.classLoader?.getResourceAsStream("local.properties")
+        properties.load(inputStream)
+        return properties.getProperty("API_KEY")
+    }
 }
+
